bcd9a5ca2ae751a057d967fc39c1841483cabbb8
Josh Marshall <catchjosh@gmail.com>
2011-02-12 12:32:59 -0600

    Incrementing version.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import distutils.core
 
 distutils.core.setup(
     name = "jsonrpclib",
-    version = "0.12",
+    version = "0.13",
     packages = ["jsonrpclib"],
     author = "Josh Marshall",
     author_email = "catchjosh@gmail.com",
