da75a402e9fbdae05c37129b2689a47ab963d508
Thomas Calmant <thomas.calmant@gmail.com>
2013-10-14 14:50:35 +0200

    Added the Apache License header to all files

--- jsonrpclib/SimpleJSONRPCServer.py
+++ jsonrpclib/SimpleJSONRPCServer.py
@@ -4,6 +4,18 @@
 Defines a request dispatcher, a HTTP request handler, a HTTP server and a
 CGI request handler.
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
 :license: Apache License 2.0
 :version: 0.1.5
 """
