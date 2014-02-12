3a54c3699bb9cd085586c61138b35b46a15ba325
Alfred Morgan <alfred@54.org>
2014-02-11 20:42:18 -0800

    mime-type application/json for Accept/Content-Type
    I like Roland's answer "reason: transport and contents are independent. so all the transport here needs to know is the format of the contents, and that's just json."
    https://groups.google.com/forum/#!topic/json-rpc/6OXOOm4fcC8

--- jsonrpclib/jsonrpc.py
+++ jsonrpclib/jsonrpc.py
@@ -117,8 +117,8 @@ class TransportMixIn(object):
     _connection = None
 
     def send_content(self, connection, request_body):
-        connection.putheader("Accept", "application/json-rpc")
-        connection.putheader("Content-Type", "application/json-rpc")
+        connection.putheader("Accept", "application/json")
+        connection.putheader("Content-Type", "application/json")
         connection.putheader("Content-Length", str(len(request_body)))
         connection.endheaders()
         if request_body:
